b5601867de3f03501757dba71e1a68c416971d5e
Jason <jason.mahdjoub@distri-mind.fr>
2019-03-13 15:35:53 +0100

    Revert "revert"
    This reverts commit 376ced20e4cdbdfa37091e1627912aa0c89ab99d.

--- MaDKitLanEdition/src/test/java/com/distrimind/madkit/kernel/network/MultipleConnectionsTest.java
+++ MaDKitLanEdition/src/test/java/com/distrimind/madkit/kernel/network/MultipleConnectionsTest.java
@@ -42,6 +42,7 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.logging.Level;
 
+import com.distrimind.madkit.testing.util.agent.NetworkPingAgent;
 import com.distrimind.ood.database.EmbeddedH2DatabaseWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -207,11 +208,11 @@ public class MultipleConnectionsTest extends JunitMadkit {
 					Assert.assertTrue(agentsToLaunch4.networkPingAgent.isOK());
 					Assert.assertTrue(agentsToLaunch5.networkPingAgent.isOK());
 
-					agentsToLaunch1.networkPingAgent.killPingPongAgents();
+					/*agentsToLaunch1.networkPingAgent.killPingPongAgents();
 					agentsToLaunch2.networkPingAgent.killPingPongAgents();
 					agentsToLaunch3.networkPingAgent.killPingPongAgents();
 					agentsToLaunch4.networkPingAgent.killPingPongAgents();
-					agentsToLaunch5.networkPingAgent.killPingPongAgents();
+					agentsToLaunch5.networkPingAgent.killPingPongAgents();*/
 
 
 					Assert.assertEquals(ReturnCode.SUCCESS, agentsToLaunch1.launchBigDataTransferAgent());
@@ -222,8 +223,8 @@ public class MultipleConnectionsTest extends JunitMadkit {
 					sleep(500);
 					for (Madkit m : getHelperInstances(5))
 					{
-						Assert.assertTrue(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, true));
-						Assert.assertTrue(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, false));
+						Assert.assertTrue(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, true, AgentBigTransfer.bigTransferRole));
+						Assert.assertTrue(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, false, AgentBigTransfer.bigTransferRole));
 					}
 
 					int nb = 0;
@@ -252,7 +253,7 @@ public class MultipleConnectionsTest extends JunitMadkit {
 					Assert.assertTrue(agentsToLaunch3.agentBigTransfer.isOK());
 					Assert.assertTrue(agentsToLaunch4.agentBigTransfer.isOK());
 					Assert.assertTrue(agentsToLaunch5.agentBigTransfer.isOK());
-					sleep(1000);
+					sleep(2000);
 					Assert.assertTrue(agentsToLaunch1.agentBigTransfer.isKilled());
 					Assert.assertTrue(agentsToLaunch2.agentBigTransfer.isKilled());
 					Assert.assertTrue(agentsToLaunch3.agentBigTransfer.isKilled());
@@ -261,8 +262,8 @@ public class MultipleConnectionsTest extends JunitMadkit {
 
 					for (Madkit m : getHelperInstances(5))
 					{
-						Assert.assertFalse(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, false));
-						Assert.assertFalse(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, true));
+						Assert.assertFalse(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, false, AgentBigTransfer.bigTransferRole));
+						Assert.assertFalse(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, AgentBigTransfer.class, true, AgentBigTransfer.bigTransferRole));
 					}
 					if (logger != null)
 						logger.info("stoping networks");
@@ -300,6 +301,12 @@ public class MultipleConnectionsTest extends JunitMadkit {
 						checkConnectedKernelsNb(this, m, 0, timeOut);
 						checkConnectedIntancesNb(this, m, 0, timeOut);
 					}
+					for (Madkit m : getHelperInstances(5))
+					{
+						Assert.assertTrue(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, NetworkPongAgent.class, true, null));
+						Assert.assertTrue(isAgentsPresentInGroup(m, JunitMadkit.DEFAULT_NETWORK_GROUP_FOR_ACCESS_DATA, NetworkPingAgent.class, true, null));
+					}
+
 					sleep(400);
 					agentsToLaunch1.killAgent(agentsToLaunch1);
 					agentsToLaunch2.killAgent(agentsToLaunch2);
