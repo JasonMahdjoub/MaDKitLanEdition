ac027a803dbe3adbbe27b48c461d85f1cc111540
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2020-02-13 14:53:30 +0100

    Update OOD to 2.4.1 Stable

--- MaDKitLanEdition/src/main/java/com/distrimind/madkit/kernel/Madkit.java
+++ MaDKitLanEdition/src/main/java/com/distrimind/madkit/kernel/Madkit.java
@@ -142,8 +142,8 @@ final public class Madkit {
 		Calendar c = Calendar.getInstance();
 		c.set(2015, Calendar.MAY, 22);
 		Calendar c2 = Calendar.getInstance();
-		c2.set(2019, Calendar.FEBRUARY, 11);
-		Version VERSION = new Version("MaDKitLanEdition", "MKLE", (short)2, (short)1, (short)8, Version.Type.Stable, (short)1, c.getTime(), c2.getTime());
+		c2.set(2019, Calendar.FEBRUARY, 13);
+		Version VERSION = new Version("MaDKitLanEdition", "MKLE", (short)2, (short)1, (short)9, Version.Type.Stable, (short)1, c.getTime(), c2.getTime());
 		try {
 
 			InputStream is = Madkit.class.getResourceAsStream("build.txt");
@@ -165,8 +165,15 @@ final public class Madkit {
 			VERSION.addDeveloper(new PersonDeveloper("Ferber", "Jacques", c.getTime()));
 
 			c = Calendar.getInstance();
+			c.set(2020, Calendar.FEBRUARY, 13);
+			Description d = new Description((short)2, (short)1, (short)9, Version.Type.Stable, (short)1, c.getTime());
+			d.addItem("Update Utils to 4.10.0");
+			d.addItem("Update OOD to 2.4.1");
+			VERSION.addDescription(d);
+
+			c = Calendar.getInstance();
 			c.set(2020, Calendar.FEBRUARY, 11);
-			Description d = new Description((short)2, (short)1, (short)8, Version.Type.Stable, (short)1, c.getTime());
+			d = new Description((short)2, (short)1, (short)8, Version.Type.Stable, (short)1, c.getTime());
 			d.addItem("Update Utils to 4.9.0");
 			d.addItem("Update OOD to 2.4.0");
 			VERSION.addDescription(d);
